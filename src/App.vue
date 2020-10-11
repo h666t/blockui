ed69d1975bbb9811e18a8cf269321805a3b8d499
h666t <1845720928@qq.com>
2020-10-11 21:36:39 +0800

    topnav update css

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <router-view/>
-  <router-link to="/xxx">xxx</router-link>
 </template>
 
 <script>
 export default {
   name: 'App',
-
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,11 @@
 <template>
   <div>hi</div>
-  <X/>
 </template>
 
 <script>
-  import X from './Frank'
 export default {
   name: 'HelloWorld',
   components:{
-    X
   },
 }
 </script>
