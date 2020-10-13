49705afceab4d20f5d42ac76d5ba49f649f07fa1
h666t <1845720928@qq.com>
2020-10-13 20:31:42 +0800

    完成button的theme样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -49,5 +49,21 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.block-theme-link{
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,&:focus{
+      color: lighten($blue, 10%);
+    }
+  }
+  &.block-theme-text{
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,&:focus{
+      background: darken(white, 5%);;
+    }
+  }
 }
 </style>
