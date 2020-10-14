bd571a9066f1751cfa4ceae40a08ae84ca39fe95
h666t <1845720928@qq.com>
2020-10-14 20:48:31 +0800

    实现DialogDemo，点击按钮显示dialog

--- src/components/doc/DialogDemo.vue
+++ src/components/doc/DialogDemo.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    <Dialog/>
+    <Dialog :visible="visible"/>
+    <Button @click="toggle">toggle</Button>
   </div>
 </template>
 
 <script lang="ts">
 import Dialog from '../../lib/Dialog.vue';
+import {ref} from 'vue'
+import Button from '../../lib/Button.vue';
   export default {
     components:{
+      Button,
       Dialog
+    },
+    setup(){
+      const visible = ref(false)
+      const toggle = () => {
+        visible.value = !visible.value
+      }
+      return {visible,toggle}
     }
   }
 </script>
