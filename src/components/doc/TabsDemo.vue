0bc6cfbb85e2087c0a8cea07d1f74d523ee2a45c
h666t <1845720928@qq.com>
2020-10-15 23:04:23 +0800

    实现Tab点击切换内容

--- src/components/doc/TabsDemo.vue
+++ src/components/doc/TabsDemo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Tabs>
+    <Tabs v-model:selected="selected">
       <Tab title="导航1">内容1</Tab>
       <Tab title="导航2">内容2</Tab>
     </Tabs>
@@ -10,8 +10,12 @@
 <script lang="ts">
 import Tabs from '../../lib/Tabs.vue';
 import Tab from '../../lib/Tab.vue';
+import {ref} from 'vue'
   export default {
     components: {Tabs, Tab},
-
+    setup(){
+      const selected = ref('导航1')
+      return {selected}
+    }
   }
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,23 +1,77 @@
 <template>
-<div>
-  <slot/>
+<div class="block-tabs">
+  <div class="block-tabs-nav">
+    <div class="block-tabs-nav-item"
+         v-for="(t,index) in titles" :key="index"
+         :class="t === selected ? 'selected' : ''"
+         @click="()=>{select(t)}"
+    >
+      {{ t }}
+    </div>
+  </div>
+  <div class="block-tabs-content">
+    <component :is="current" :key="selected" class="block-tabs-content-item"/>
+  </div>
+
 </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue'
+import {computed} from 'vue'
   export default {
-    setup(props,context){
-      const tagList = context.slots.default()
-      tagList.forEach((tag)=>{
-        if (tag.type !== Tab){
-          throw new Error('Tabs 的内部必须是 Tab')
+    props: {
+      selected: String,
+    },
+    setup(props, context) {
+      const defaults = context.slots.default();
+      defaults.forEach((tag) => {
+        if (tag.type !== Tab) {
+          throw new Error('Tabs 的内部必须是 Tab');
         }
+      });
+      const titles = defaults.map(item => {
+        return item.props.title;
+      });
+      const select = (title: string) => {
+        context.emit('update:selected',title)
+        return defaults.filter(item=> item.props.title === title)[0]
+      }
+      const current = computed(()=>{
+        return select(props.selected)
       })
+      return {defaults, titles,select,current};
     }
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.block-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
 
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+  }
+}
 </style>
