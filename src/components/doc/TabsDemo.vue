6cfadb89cb285da3cdcbc664a6a76c4c6066fa16
h666t <1845720928@qq.com>
2020-10-15 19:42:18 +0800

    完成Tabs内部元素的è§检查

--- src/components/doc/TabsDemo.vue
+++ src/components/doc/TabsDemo.vue
@@ -1,10 +1,17 @@
 <template>
-  <div><Tabs/></div>
+  <div>
+    <Tabs>
+      <Tab title="导航1">内容1</Tab>
+      <Tab title="导航2">内容2</Tab>
+    </Tabs>
+  </div>
 </template>
 
 <script lang="ts">
 import Tabs from '../../lib/Tabs.vue';
+import Tab from '../../lib/Tab.vue';
   export default {
-    components:{Tabs}
+    components: {Tabs, Tab},
+
   }
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,20 @@
 <template>
-<div>Tabs组件2</div>
+<div>
+  <slot/>
+</div>
 </template>
 
 <script lang="ts">
+import Tab from './Tab.vue'
   export default {
-
+    setup(props,context){
+      const tagList = context.slots.default()
+      tagList.forEach((tag)=>{
+        if (tag.type !== Tab){
+          throw new Error('Tabs 的内部必须是 Tab')
+        }
+      })
+    }
   }
 </script>
 
