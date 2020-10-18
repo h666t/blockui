c923e3d4b241742f7f2aa53a9be45c3a5cbe3d22
h666t <1845720928@qq.com>
2020-10-18 18:12:49 +0800

    展示源代码

--- src/components/doc/Install.vue
+++ src/components/doc/Install.vue
@@ -1,6 +1,10 @@
 <template>
-<div>
-  install
-</div>
+<article class="markdown-body">
+  <h2><strong>安装</strong></h2>
+  <pre><code>npm install blockui</code></pre>
+  <p>或</p>
+  <pre><code>yarn add blockui</code></pre>
+  下一节：<a href="/#/doc/getStart">开始使用</a>
+</article>
 </template>
 
--- src/components/doc/SwitchDemo.vue
+++ src/components/doc/SwitchDemo.vue
@@ -1,17 +1,50 @@
 <template>
-<div>
-  <Switch v-model:value="checked" />
+<div class="switchDemo-wrapper">
+  <h2>Switch 开关</h2>
+  <br>
+  <h3>何时使用</h3>
+  <p>需要表示开关状态/两种状态之间的切换时；</p>
+  <br>
+  <h3>代码演示</h3>
+  <div class="demo1-box">
+    <h4>基本用法</h4>
+    <div class="intro">
+      <div class="demo">
+        <SwitchDemo1/>
+      </div>
+      <Button size="small">展示代码</Button>
+      <pre>
+        {{SwitchDemo1.__sourceCode}}
+      </pre>
+    </div>
+  </div>
+
 </div>
 </template>
 
 <script lang="ts">
-import Switch from '../../lib/Switch.vue';
-import {ref} from 'vue'
+import SwitchDemo1 from './SwitchDemo1.vue';
+import Button from '../../lib/Button.vue';
 export default {
-  components:{Switch},
+  components: {Button, SwitchDemo1},
   setup(){
-    const checked = ref(false)
-    return {checked}
+    return{SwitchDemo1}
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.switchDemo-wrapper{
+  >.demo1-box{
+    display: grid;
+    border:1px solid rgba(0,0,0,0.25);
+    >.demo{
+      padding: 8px;
+    }
+    >.intro{
+      border-top: 1px solid rgba(0,0,0,0.25);
+      overflow: auto;
+    }
+  }
+}
+</style>
